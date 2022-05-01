7b9c165a4648e1c528b6753961aa0b39901c9e60
Jacqueline Nguyen <71247316+jnguy450@users.noreply.github.com>
2022-04-30 19:40:01 -0700

    Add files via upload

--- BlockCiphers/blockEncryptions.py
+++ BlockCiphers/blockEncryptions.py
@@ -1,10 +1,14 @@
 import random
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
 
 blockSizeBytes = 16
 
+
 def padByteArray(array):
-    if(len(array) % blockSizeBytes != 0):
-        newLen = (len(array)//blockSizeBytes + 1) * blockSizeBytes
+    if (len(array) % blockSizeBytes != 0):
+        newLen = (len(array) // blockSizeBytes + 1) * blockSizeBytes
         addedBytes = newLen - len(array)
         padding = bytearray(addedBytes.to_bytes(1, 'little')) * addedBytes
         # print(padding)
@@ -13,8 +17,6 @@ def padByteArray(array):
     else:
         return array
 
-def generateRandomByteKey(length):
-    return [random.randint(0, 255) for _ in range(length)]
 
 def xorByteArray(arr1, arr2):
     if len(arr1) != len(arr2):
@@ -25,19 +27,24 @@ def xorByteArray(arr1, arr2):
         newArray.append(arr1[i] ^ arr2[i])
     return newArray
 
+
 def ECBEncryptBinary(key, plaintext):
     if len(key) != blockSizeBytes:
-        print("ERROR: key is not of length " + blockSizeBytes)
+        print("ERROR: key is not of length " + str(blockSizeBytes))
         return
 
     plaintext = padByteArray(plaintext)
 
+    cipher = Cipher(algorithms.AES(key), modes.ECB())
+    encryptor = cipher.encryptor()
     cipherText = bytearray()
 
-    for i in range(0, len(plaintext)//blockSizeBytes):
-        cipherText += xorByteArray(plaintext[i*blockSizeBytes: i*blockSizeBytes+blockSizeBytes], key)
-    
+    for i in range(0, len(plaintext) // blockSizeBytes):
+        cipherTextBuf = encryptor.update(plaintext[i * blockSizeBytes: i * blockSizeBytes + blockSizeBytes])
+        cipherText += cipherTextBuf
+
     return cipherText
+
 
 def CBCEncryptBinary(key, initVector, plaintext):
     if len(key) != blockSizeBytes:
@@ -47,32 +54,37 @@ def CBCEncryptBinary(key, initVector, plaintext):
         print("ERROR: key is not of length " + blockSizeBytes)
         return
 
-    # print(plaintext)
     plaintext = padByteArray(plaintext)
-    
+
+    cipher = Cipher(algorithms.AES(key), modes.ECB())
+    encryptor = cipher.encryptor()
     cipherText = bytearray()
+    lastBlock = bytearray(initVector)
 
-    lastBlock = initVector
-
-    for i in range(0, len(plaintext)//blockSizeBytes):
-        preCipherText = xorByteArray(plaintext[i*blockSizeBytes: i*blockSizeBytes+blockSizeBytes], lastBlock)
-        newCipherText = xorByteArray(preCipherText, key)
-        lastBlock = newCipherText
+    for i in range(0, len(plaintext) // blockSizeBytes):
+        prePlainText = plaintext[i * blockSizeBytes: i * blockSizeBytes + blockSizeBytes]
+        preCipherText = xorByteArray(prePlainText, lastBlock)
+        cipherTextBuf = encryptor.update(preCipherText)
+        lastBlock = cipherTextBuf
         cipherText += lastBlock
-    
+
     return cipherText
+
 
 def ECBDecryptBinary(key, cipherText):
     if len(key) != blockSizeBytes:
         print("ERROR: key is not of length " + blockSizeBytes)
         return
-    
-    plaintext = bytearray()
 
-    for i in range(0, len(cipherText)//blockSizeBytes):
-        plaintext += xorByteArray(cipherText[i*blockSizeBytes: i*blockSizeBytes+blockSizeBytes], key)
+    plaintext = bytearray()
+    cipher = Cipher(algorithms.AES(key), modes.ECB())
+    decryptor = cipher.decryptor()
+
+    for i in range(0, len(cipherText) // blockSizeBytes):
+        plaintext += decryptor.update(cipherText[i * blockSizeBytes: i * blockSizeBytes + blockSizeBytes])
 
     return plaintext
+
 
 def CBCDecryptBinary(key, initVector, cipherText):
     if len(key) != blockSizeBytes:
@@ -81,27 +93,29 @@ def CBCDecryptBinary(key, initVector, cipherText):
     if len(initVector) != blockSizeBytes:
         print("ERROR: key is not of length " + blockSizeBytes)
         return
-    
-    plainText = bytearray()
 
+    plainText = bytearray()
+    cipher = Cipher(algorithms.AES(key), modes.ECB())
+    decryptor = cipher.decryptor()
     lastBlock = initVector
 
-    for i in range(0, len(cipherText)//blockSizeBytes):
-        prePlaintext = xorByteArray(cipherText[i*blockSizeBytes: i*blockSizeBytes+blockSizeBytes], key)
+    for i in range(0, len(cipherText) // blockSizeBytes):
+        prePlaintext = decryptor.update(cipherText[i * blockSizeBytes: i * blockSizeBytes + blockSizeBytes])
         newPlaintext = xorByteArray(prePlaintext, lastBlock)
-        lastBlock = cipherText[i*blockSizeBytes: i*blockSizeBytes+blockSizeBytes]
+        lastBlock = cipherText[i * blockSizeBytes: i * blockSizeBytes + blockSizeBytes]
         plainText += newPlaintext
-    
+
     return plainText
 
+
 def encryptBMP(filepath):
-    key1 = generateRandomByteKey(blockSizeBytes)
-    key2 = generateRandomByteKey(blockSizeBytes)
-    initVector = generateRandomByteKey(blockSizeBytes)
+    key1 = os.urandom(blockSizeBytes)
+    key2 = os.urandom(blockSizeBytes)
+    initVector = os.urandom(blockSizeBytes)
 
     with open(filepath, 'rb') as ecbInput:
         txt = bytearray(ecbInput.read())
-    
+
     # print(txt)
     header = txt[0:54]
     ecbEncryptedMessage = ECBEncryptBinary(key1, txt)
@@ -109,7 +123,7 @@ def encryptBMP(filepath):
 
     with open('ECB.bmp', 'wb+') as file:
         file.write(ecbEncryptedMessage)
-    
+
     # print(txt)
     header = txt[0:54]
     cbcEncryptedMessage = CBCEncryptBinary(key2, initVector, txt)
@@ -118,30 +132,43 @@ def encryptBMP(filepath):
     with open('CBC.bmp', 'wb+') as file:
         file.write(cbcEncryptedMessage)
 
+
+# Doesn't work yet
 def encryptFile(filepath):
-    key1 = generateRandomByteKey(blockSizeBytes)
-    key2 = generateRandomByteKey(blockSizeBytes)
-    initVector = generateRandomByteKey(blockSizeBytes)
+    key1 = os.urandom(blockSizeBytes)
+    key2 = os.urandom(blockSizeBytes)
+    initVector = os.urandom(blockSizeBytes)
 
     with open(filepath, 'rb') as ecbInput:
         txt = bytearray(ecbInput.read())
-    
+
+    # print(txt)
+    header = txt[0:54]
     ecbEncryptedMessage = ECBEncryptBinary(key1, txt)
-    with open('ECB', 'wb+') as file:
+    ecbEncryptedMessage = header + ecbEncryptedMessage[54:]
+
+    with open('ECB.bmp', 'wb+') as file:
         file.write(ecbEncryptedMessage)
 
+    # print(txt)
+    header = txt[0:54]
     cbcEncryptedMessage = CBCEncryptBinary(key2, initVector, txt)
-    with open('CBC', 'wb+') as file:
+    cbcEncryptedMessage = header + cbcEncryptedMessage[54:]
+
+    with open('CBC.bmp', 'wb+') as file:
         file.write(cbcEncryptedMessage)
 
-    ecbDecryptedMessage = ECBDecryptBinary(key1, ecbEncryptedMessage)
-    cbcDecryptedMessage = CBCDecryptBinary(key2, initVector, cbcEncryptedMessage)
+    ecbDecryptedMessage = ECBDecryptBinary(key1, ecbEncryptedMessage[54:])
+    cbcDecryptedMessage = CBCDecryptBinary(key2, initVector, cbcEncryptedMessage[54:])
 
-    print(ecbDecryptedMessage == txt)
-    print(cbcDecryptedMessage == txt)
+    with open('ECBdec.bmp', 'wb+') as file:
+        file.write(header + ecbDecryptedMessage)
+
+    with open('CBCdec.bmp', 'wb+') as file:
+        file.write(header + cbcDecryptedMessage)
 
 
-# encryptFile('testInput')
+encryptFile('cp-logo.bmp')
 # encryptBMP('cp-logo.bmp')
 
 # print(padPlaintext("ABCDEF").encode('ascii'))
