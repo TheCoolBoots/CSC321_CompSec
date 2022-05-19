c7b93083d2ffcdfb2b72e0ae63314a77ec6bc13c
Andrew Lai <arjlai221@gmail.com>
2022-05-19 11:06:30 -0700

    natas PHP

--- Hashing/Hashing.py
+++ Hashing/Hashing.py
@@ -38,32 +38,32 @@ def task1_c(hashWidth):
         if currentHash not in attemptedHashes:
             attemptedHashes[currentHash] = currentNum
         elif attemptedHashes[currentHash] != currentNum:
-            print(f'{currentNum} and {attemptedHashes[currentHash]} both share the first {hashWidth} bits: {currentHash}')
+            # print(f'{currentNum} and {attemptedHashes[currentHash]} both share the first {hashWidth} bits: {currentHash}')
             break
         
         currentNum += 1
 
     endTime = t.datetime.now()
-    print(f'Time elapsed = {endTime - startTime}')
+    # print(f'Time elapsed = {endTime - startTime}')
     print(f'Number of inputs tried = {currentNum}')
-    print(f'Datapoint for graph: ({hashWidth}, {endTime - startTime})')
+    print(f'{hashWidth}, {endTime - startTime}')
     return hashWidth, endTime - startTime, currentNum
 
 def task1_c_generateData():
     x = []
     y = []
     z = []
-    for i in range(8, 50):
-        print(f'Digest Size = {i}')
+    for i in range(39, 51):
+        # print(f'Digest Size = {i}')
         a = task1_c(i)
-        x.append(a[0])
-        y.append(a[1].total_seconds())
-        z.append(a[2])
-    plt.subplot(1, 2 ,1)
-    plt.plot(x, y, '-')
-    plt.subplot(1, 2, 2)
-    plt.plot(x, z, '-')
-    plt.show()
+        # x.append(a[0])
+        # y.append(a[1].total_seconds())
+        # z.append(a[2])
+    # plt.subplot(1, 2 ,1)
+    # plt.plot(x, y, '-')
+    # plt.subplot(1, 2, 2)
+    # plt.plot(x, z, '-')
+    # plt.show()
 
 def importUserHashes(filepath):
     with open(filepath, 'rb') as inputFile:
@@ -77,3 +77,37 @@ def importUserHashes(filepath):
 
 
 task1_c_generateData()
+
+"""
+8, 0:00:00.001136
+9, 0:00:00.000238
+10, 0:00:00.000377
+11, 0:00:00.000335
+12, 0:00:00.000289
+13, 0:00:00.000322
+14, 0:00:00.002197
+15, 0:00:00.002100
+16, 0:00:00.002393
+17, 0:00:00.001805
+18, 0:00:00.005223
+19, 0:00:00.005636
+20, 0:00:00.015409
+21, 0:00:00.042417
+22, 0:00:00.042848
+23, 0:00:00.055597
+24, 0:00:00.187713
+25, 0:00:00.177291
+26, 0:00:00.176393
+27, 0:00:00.176875
+28, 0:00:01.501429
+29, 0:00:01.996534
+30, 0:00:01.998325
+31, 0:00:21.199042
+32, 0:00:20.461063
+33, 0:00:20.763102
+34, 0:01:19.221674
+35, 0:02:46.703342
+36, 0:17:23.859476
+37, 0:16:42.442957
+38, 1:22:16.198924
+"""
